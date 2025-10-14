cb9c67ce52a97dd43962351a3a69d937a7961273
Muhammed Ali CAN <canpacis@gmail.com>
2025-10-14 15:46:56 +0300

    fix: update asset output name

--- vite.config.ts
+++ vite.config.ts
@@ -29,9 +29,9 @@ export default defineConfig({
     cssCodeSplit: true,
     rollupOptions: {
       output: {
-        entryFileNames: "assets/[name].[hash].js",
-        chunkFileNames: "assets/[name].[hash].js",
-        assetFileNames: "assets/[name].[hash].[ext]",
+        entryFileNames: "[name].[hash].js",
+        chunkFileNames: "[name].[hash].js",
+        assetFileNames: "[name].[hash].[ext]",
       },
     },
   },
